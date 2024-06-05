b67fe8737f9517173a27978ecbc2f945ef42db39
Moises Alvarez <mfalvare@espol.edu.ec>
2024-06-05 15:00:02 -0500

    usando firbase

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
-let loaded = ( eventLoaded ) => {
+let loaded = (eventLoaded) => {
   
-    window.alert("landing page loaded");
-    console.log( eventLoaded );
-    
-    let myform = document.getElementById('form');
-    
-    myform.addEventListener('submit', ( eventSubmit ) => { 
-  
-      debugger;
-  
-    })
-  
-  }
+  console.log(eventLoaded);
 
-window.addEventListener("DOMContentLoaded", loaded)
+  let formulario = document.getElementById("formulario");
+  let name = document.getElementById("name");
+  let apellido = document.getElementById("apellido");
+  let email = document.getElementById("email");
+  let password = document.getElementById("contraseña");
+  let conf_password = document.getElementById("contraseña-confirma");
+  let genero = document.getElementById("genero");
+  
+
+
+  formulario.addEventListener("submit", (eventSubmit) => {
+    eventSubmit.preventDefault();
+
+    if (name.value.length == 0) {
+      alert("Nombre requerido");
+      form_name.focus();
+      return;
+    }
+    const datos = {
+      nombre: name.value +" "+apellido.value,
+      email: email.value,
+      password: password.value
+  };
+
+  fetch('https://productos-d4a40-default-rtdb.firebaseio.com/collection.json', {
+      method: 'POST',
+      body: JSON.stringify(datos),
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  })
+  .then(respuesta => respuesta.json())
+  .then(datos => {
+      console.log(datos); // Imprimir la respuesta del servidor
+  })
+  .catch(error => console.error(error));
+
+    debugger;
+  });
+};
+
+window.addEventListener("DOMContentLoaded", loaded);
